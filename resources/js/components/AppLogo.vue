8f210e1481b6351396164bbcb007f1be79e964b5
owenbcoding <eoghankb@gmail.com>
2026-03-05 17:16:43 +0000

    Created App logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { Terminal } from 'lucide-vue-next';
 </script>
 
 <template>
     <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
+        class="flex aspect-square size-8 items-center justify-center rounded-md bg-primary/15"
     >
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+        <Terminal class="size-5 text-primary" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold"
-            >Laravel Starter Kit</span
+            >HackerInn</span
         >
     </div>
 </template>
